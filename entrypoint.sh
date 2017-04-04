9f0fcc526879ddc19915290fa30b73c260fbc193
Ron Kurr <kurr@kurron.org>
2017-04-04 17:25:07 -0400

    Fixed a typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ INSTANCE_ID=$(curl --location http://169.254.169.254/latest/meta-data/instance-i
 
 echo $(date): $(/usr/local/bin/aws --version)
 echo "Polling ${NOTICE_URL} every ${POLL_INTERVAL} second(s)"
-echo "Will generate notification event for instance ${INSTNACE_ID} once a termination event is detected."
+echo "Will generate notification event for instance ${INSTANCE_ID} once a termination notice is detected."
 
 while http_status=$(curl --output /dev/null --write-out '%{http_code}' --silent --location ${NOTICE_URL}); [ ${http_status} -ne 200 ]; do
   echo $(date): ${http_status}
